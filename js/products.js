45908faa0ccbb10a56aa5c6712b0b737e3e59616
nickyshone62-ship-it <nickyshone62@gmail.com>
2026-08-16 13:17:03 +0200

    Update style for Oud Nuclear from Doux to Puissant

--- js/products.js
+++ js/products.js
@@ -85,7 +85,7 @@ const PRODUCTS_DATA = [
     name: "Oud Nuclear",
     gender: "homme",
     genderLabel: "Homme",
-    style: "Doux",
+    style: "Puissant",
     price: 12000,
     size: "100 ml",
     image: "images/oud-nuclear.png",
